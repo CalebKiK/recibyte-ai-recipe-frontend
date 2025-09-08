b389e3640934dbdabbcb6bc6bb3d3493f2f5fca1
Caleb Karimi <karimicaleb@gmail.com>
2025-09-08 20:45:31 +0300

    Adding allowed image sources in config file and add margn on image in recipe choice component

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,36 @@ const nextConfig = {
     //     return [
     //         {
     //             source: '/api/:path*',
-    //             destination: 'https://backend-recipbyte.fly.dev/api/:path*',
+    //             destination: 'http://127.0.0.1:8000/api/:path*',
     //         },
     //     ];
     // },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "img.spoonacular.com",
+                pathname: "/**", 
+            },
+            {
+                protocol: "http",
+                hostname: "127.0.0.1",
+                port: "8000",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "backend-recipbyte.fly.dev",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "img.sndimg.com",
+                pathname: "/**",
+            },
+        ],
+    }
 };
 
 export default nextConfig;

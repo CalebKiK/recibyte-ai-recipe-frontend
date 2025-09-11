9ab4ac06daddd003078367b106c865373442516c
CalebKiK <karimicaleb@gmail.com>
2025-09-11 09:17:34 +0300

    Add deployed backend url in the files in the src/api folder

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:800/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://backend-recipbyte.fly.dev/api";
+// const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:800/api";
 
 // 🔹 Filter recipes by ingredients + dietary restrictions
 export async function getRecipesByIngredients(ingredients, dietaryRestrictions) {
--- src/api/users.js
+++ src/api/users.js
@@ -1,1 +1,2 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:800/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://backend-recipbyte.fly.dev/api";
+// const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api";

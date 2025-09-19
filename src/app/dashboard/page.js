afdbab0d741b882a285bb99f0938fee67d4ae5b9
CalebKiK <karimicaleb@gmail.com>
2025-09-19 11:23:04 +0300

    Sort of protect the dashboard page from unauthenticated users

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 import Navbar from "@/components/shared/Navbar";
 import UserDashboard from "@/components/dashboard/UserDashboard";
+import toast from "react-hot-toast";
 
 export default function DashboardPage() {
+
+    const { token } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!token) {
+            toast.error("You must sign in to access dashboard.");
+            router.push("/authentication");
+        }
+    }, [token, router]);
+
+    if (!token) {
+        return null;
+    }
+
     return(
         <div className="dashboard-page">
             <Navbar />

2ed82876d228d06a2e549c8e4802bcb178df8085
CalebKiK <karimicaleb@gmail.com>
2025-09-24 16:46:42 +0300

    Update meal plan component and scehdule to improve UI/UX of the component and the schedule table

--- src/components/dashboard/meal-planner/MealPlanner.js
+++ src/components/dashboard/meal-planner/MealPlanner.js
@@ -22,9 +22,9 @@ export default function MealPlanner() {
     return (
         <div className="meal-planner-component">
             <div className='meal-planner-header'>
-                <h2>Your Weekly Menu / Meal Prep Lab</h2>
+                <h3>Your Weekly Menu / Meal Prep Lab</h3>
                 {/* Button below conditionally rendered if there is no meal plan or if it is 3 days to the end of current meal plan */}
-                <button onClick={openModal}>Add Meal Plan</button>
+                <button className="add-meal-plan-btn" onClick={openModal}>Add Meal Plan</button>
             </div>
             
             <p>A week of delicious decisions, all in one place. We do the math, you make the magic.</p>

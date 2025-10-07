60848fe9c548c960f0331c9983eaf72a58167d72
Caleb Karimi <karimicaleb@gmail.com>
2025-10-07 11:40:51 +0300

    Clean up auth file to remove unused code

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,7 @@
 "use client";
 
 import { createContext, useContext, useState, useEffect } from "react";
-import { jwtDecode } from "jwt-decode";
-import { fetchUserByToken, fetchUserProfile } from "@/api/users";
-import { refreshAccessToken } from "@/api/token/refresh";
+import { fetchUserProfile } from "@/api/users";
 import { BASE_URL } from "@/api/config";
 
 const AuthContext = createContext();
@@ -12,40 +10,6 @@ export function AuthProvider({ children }) {
     const [token, setToken] = useState(null);
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-
-    // useEffect(() => {
-    //     const initAuth = async () => {
-    //     let access = localStorage.getItem("access");
-    //     const refresh = localStorage.getItem("refresh");
-
-    //     if (!access && refresh) {
-    //         // No access, but refresh available → try to refresh
-    //         try {
-    //         access = await refreshAccessToken();
-    //         setToken(access);
-    //         localStorage.setItem("access", access);
-    //         } catch {
-    //         logout();
-    //         return;
-    //         }
-    //     }
-
-    //     if (access) {
-    //         try {
-    //         setToken(access);
-    //         const decoded = jwtDecode(access);
-    //         // Optionally fetch user profile from backend
-    //         const userData = await fetchUserByToken(access);
-    //         setUser(userData || decoded);
-    //         } catch (err) {
-    //         console.error("Invalid token at init", err);
-    //         logout();
-    //         }
-    //     }
-    //     };
-
-    //     initAuth();
-    // }, []);
 
     useEffect(() => {
         const initAuth = async () => {
@@ -80,26 +44,6 @@ export function AuthProvider({ children }) {
         initAuth();
     }, []);
 
-    // const login = async ({ access, refresh }) => {
-    //     // console.log("Received token in login:", newToken);
-    //     // localStorage.setItem("authToken", newToken);
-    //     // setToken(newToken);
-
-    //     localStorage.setItem("access", access);
-    //     localStorage.setItem("refresh", refresh);
-    //     setToken(access);
-
-    //     try {
-    //         const decoded = jwtDecode(access); 
-    //         const userData = await fetchUserByToken(access);
-    //         setUser(userData || decoded);
-            
-    //     } catch (err) {
-    //         console.error("Invalid login token", err);
-    //         logout();
-    //     }
-    // };
-
     const login = async (email, password) => {
         const res = await fetch(`${BASE_URL}/token/`, {
             method: "POST",
@@ -117,14 +61,6 @@ export function AuthProvider({ children }) {
         setUser(userData);
     };
 
-    // const logout = async () => {
-    //     // localStorage.removeItem("authToken");
-    //     localStorage.removeItem("access");
-    //     localStorage.removeItem("refresh");
-    //     setToken(null);
-    //     setUser(null);
-    // };
-
     const logout = async () => {
         setUser(null);
         // Optional: hit a backend logout endpoint that clears cookies
@@ -135,9 +71,6 @@ export function AuthProvider({ children }) {
     };
 
     return (
-        // <AuthContext.Provider value={{ token, user, login, logout }}>
-        //     {children}
-        // </AuthContext.Provider>
         <AuthContext.Provider value={{ user, token, login, logout, loading }}>
             {children}
         </AuthContext.Provider>
